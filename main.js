2be8c86b7c297bd78e863016244d434b9af77dbe
KennethConcepcionKOC <concepcion.ko@obmontessori.edu.ph>
2026-03-10 13:46:59 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+// Javascript Conditionals
+/*function identify_sign() {
+
+    var num1 = document.getElementById("input1").value;
+
+    if (num1 > 0) {
+        document.getElementById("output").innerHTML = "It is positive";
+    } else if (num1 == 0) {
+        document.getElementById("output").innerHTML = "It is zero";
+    } else {
+        document.getElementById("output").innerHTML = "It is negative";
+    }
+}*/
+
+function odd_or_even() {
+
+    num1 = document.getElementById("input1").value;
+    
+    if (num1 % 2 == 0) {
+        window.alert("It is even");
+    } else {
+        window.alert("It is odd");
+    }
+
+}
